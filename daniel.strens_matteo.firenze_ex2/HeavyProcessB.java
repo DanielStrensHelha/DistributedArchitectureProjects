11bdd271248ae8f3585fd3e19aac60089d5ca1e6
MatteoFirenze <la219757@student.helha.be>
2023-10-27 12:09:10 +0200

    Implemented some method in heavyProcessB and lightWeightProcessB

--- daniel.strens_matteo.firenze_ex2/HeavyProcessB.java
+++ daniel.strens_matteo.firenze_ex2/HeavyProcessB.java
@@ -3,28 +3,74 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
 
 public class HeavyProcessB {
-    private static ServerSocket serverSocket;
     private static Socket socket;
-    private static DataInputStream in;
+    private static ServerSocket server;
     private static DataOutputStream out;
-    private static int token = 0;
+    private static DataInputStream in;
+    private static int token;
+    private static Socket[] lightWeights;
 
+    private static int answersfromLightweigth;
+    
+    private final static String SUB_PROCESS = "LightWeightProcessB";
+    private final static int LIGHT_WEIGHT_PORT = 5002;
+    private final static int NUM_LIGHTWEIGHTS = 3;
+
+     /**
+     * Connects to ProcessA and creates sub processes
+     */
     public HeavyProcessB() {
+        token = 1;
+        answersfromLightweigth = 0;
+        lightWeights = new Socket[NUM_LIGHTWEIGHTS];
+        
+        // Connect to the HeavyWeightProcess and create a server socket for the light weight ones
         try {
-            //Waiting for HeavyProcess A to connect
-            serverSocket = new ServerSocket(5000);
+            socket = new Socket("localhost", 5002);
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+
+            server = new ServerSocket(LIGHT_WEIGHT_PORT);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try {
+            createProcesses();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Create the subprocesses LightWeightProcessA
+     * @throws URISyntaxException
+     */
+    private void createProcesses() throws URISyntaxException {
+        for (int i = 0; i < NUM_LIGHTWEIGHTS; i++) {
+            try {
+                // Create the process
+                ProcessBuilder pBuilder = new ProcessBuilder("java", SUB_PROCESS, Integer.toString(LIGHT_WEIGHT_PORT));
+                @SuppressWarnings("unused")
+                Process process = pBuilder.start();
+
+                // Wait for it to connect and store it into the lightweights array
+                lightWeights[i] = server.accept();
+                System.out.println("Accepted a process :D");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void startListening() {
         try {
             System.out.println("Waiting for other process to connect...");
-            socket = serverSocket.accept();
+            socket = server.accept();
             System.out.println("Connected");
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
