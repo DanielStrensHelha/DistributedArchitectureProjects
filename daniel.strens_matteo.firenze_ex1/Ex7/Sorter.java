088c9195a7244be03904c29fc9f4e3ef3b068cd5
Daniel Strens <LA217024@student.helha.be>
2023-12-01 11:40:45 +0100

    removed unused variable 'NumberOfThreads' (or smth)

--- daniel.strens_matteo.firenze_ex1/Ex7/Sorter.java
+++ daniel.strens_matteo.firenze_ex1/Ex7/Sorter.java
@@ -2,13 +2,10 @@ package Ex7;
 
 import java.security.InvalidParameterException;
 import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 public class Sorter {
-    static ExecutorService executor = Executors.newFixedThreadPool(1000);
     private static final ForkJoinPool pool = new ForkJoinPool();
     private static int[] aux;
 
